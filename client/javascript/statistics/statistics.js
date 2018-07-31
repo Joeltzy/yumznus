d31d28be59d416e76921c8ef26da74dad33e9d52
Joel Tay <joeltay95@gmail.com>
2018-07-31 12:45:52 +0800

    animated counter

--- client/javascript/statistics/statistics.js
+++ client/javascript/statistics/statistics.js
@@ -1,25 +1,75 @@
 Template.stats.helpers({
-        tolal_count: function() {
+
+        total_count: function() {
             var arrlist = new Array();
             var count_list = Votes.find().fetch();
 
             for (i = 0; i < Votes.find().count(); i++) {
                 arrlist.push(count_list[i].count);
             }
-            
             const arrSum = arr => arr.reduce((a,b) => a+b, 0)
-            return arrSum(arrlist);
+
+var total_counter = arrSum(arrlist);
+$({countNum: $('#v_count').text()}).animate({countNum: total_counter}, {
+      duration: 1300, 
+      easing:'linear',
+      step: function() {
+        $('#v_count').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#v_count').text(total_counter);
+      }
+    });
+
+
+
+
+           //return arrSum(arrlist);
         }, 
+
+
+
 
         stall_count: function() {
-            return Votes.find().count();
+
+var stall_counter = Votes.find().count();
+$({countNum: $('#tata').text()}).animate({countNum: stall_counter}, {
+
+      duration: 1300, 
+      easing:'linear',
+      step: function() {
+        $('#s_count').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#s_count').text(stall_counter);
+      }
+
+    })
+
         }, 
 
+
+
+
+
+
         rev_count: function() {
+        var rev_counter = FFrevs.find().count() +  FCrevs.find().count() + TPrevs.find().count() + DECKrevs.find().count() + FRrevs.find().count() + ACrevs.find().count();    
             
-            return FFrevs.find().count() + + FCrevs.find().count() + TPrevs.find().count() + DECKrevs.find().count() + FRrevs.find().count() + ACrevs.find().count();; 
-            
+        $({countNum: $('#r_count').text()}).animate({countNum: rev_counter}, {
+      duration: 1300, 
+      easing:'linear',
+      step: function() {
+        $('#r_count').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#r_count').text(rev_counter);
+      }
+    }); 
         },
 });
 
 
+
+
+// console.log(Votes.find().count());
