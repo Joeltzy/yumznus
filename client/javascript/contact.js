bf7ff2436c506faad8bf95b9ffc9ebd6201b5e67
Joel Tay <joeltay95@gmail.com>
2018-08-03 15:40:35 +0800

    contact form changes

--- client/javascript/contact.js
+++ client/javascript/contact.js
@@ -1,12 +1,29 @@
+var count = 5;
+
 Template.contact.events({
   'submit form': function(event){
       event.preventDefault();
+
       var username = event.target.username.value;
       var email = event.target.email.value;
       var bot1st = event.target.bot1st.value;
       var botcheck = event.target.botcheck.value;
       if (bot1st != botcheck) {
-        alert("Verification Code incorrect. Please enter the correct code!");
+        count -= 1;
+        if (count >= 3) {
+          alert("Verification Code incorrect. " + count + " tries left.");
+        }
+        else if (count == 2) {
+          alert(count + " tries left. No Brute Force please.");
+        }
+        else if (count == 1) {
+          alert("Hello Intelligent Bot. You have " + count + " try left!");
+        }
+        else if (count <= 0) {
+          alert("Sorry, you failed the Verification Test. Page will reload with a new code.")
+          location.reload();
+        }
+        
       }
 
       else if (!Match.test(email, ValidEmail)) {
@@ -14,21 +31,24 @@ Template.contact.events({
       }
 
       else {
-        alert("Response sent. Expect a reply from us within 3 working days. Thank you and have a great day!");
+        alert("Thank you " + username + ". Expect a reply from us at " + email + " within 3 working days. Have a great day!");
+
         window.location.href = '/';
       }
-
   }
 });
+
 
 Template.contact.helpers({
   makeid: function () {
     var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++)
+  for (var i = 0; i < 6; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
   }
 });
+
+
--- server/server.js
+++ server/server.js
@@ -9,4 +9,7 @@ Meteor.methods({
 
 	}
 });
+
+
+
 }
