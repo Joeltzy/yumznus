17c0f369f137d5d362a81d495ddaf43da766733c
Joel Tay <joeltay95@gmail.com>
2018-08-08 12:54:37 +0800

    Application Structure

--- client/main.js
+++ client/main.js
@@ -1,9 +1,2 @@
-
-// import '/imports/main.js';
-// import '/imports/breakpoints.min.js';
-// import '/imports/jquery.min.js';
-// import '/imports/browser.min.js';
-// import '/imports/util.js';
-
 import '../imports/startup/client';
 
